9df61f2e7acfbf93383a9196825991c38743a140
Abhinav Gupta <agupta50@mba-i1.uncc.edu>
2020-11-06 17:35:36 -0500

    lcs

--- assignment-openmp-task/lcs/lcs.cpp
+++ assignment-openmp-task/lcs/lcs.cpp
@@ -80,9 +80,9 @@ int main (int argc, char* argv[]) {
 
 		int granularity = 500;
 		if(n<=10)
-			granularity = 5;
-	       else if (n <=100)
 			granularity = 10;
+	       else if (n <=100)
+			granularity = 50;
 		else 
 			granularity = 5*n*0.01;
 
