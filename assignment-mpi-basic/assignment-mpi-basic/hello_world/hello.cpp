64907d890cc65235f695388385d7afc3dff8bd8f
abhinavg18 <abhinavvk@gmail.com>
2020-11-16 16:55:41 -0500

    helloworld

--- assignment-mpi-basic/assignment-mpi-basic/hello_world/hello.cpp
+++ assignment-mpi-basic/assignment-mpi-basic/hello_world/hello.cpp
@@ -6,12 +6,13 @@ int main(int argc, char*argv[]) {
 	
 	int rank;
 	int size;
-	char mname[128] = ""; //machine name
+	
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	char mname[128] = ""; //machine name
 	gethostname(mname, sizeof(mname));
-	printf("I am process %d out of %d. I am running on %s\n", rank, size, myhostname);
+	printf("I am process %d out of %d. I am running on %s\n", rank, size, mname);
 	MPI_Finalize();
 
 	return 0;
